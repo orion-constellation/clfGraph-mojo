576e0486192aebfce8f8681bf75ff64588509e77
orionhunts-ai <orion@oriondefensiveai.com>
2024-08-29 17:52:21 +0930

    Sklearn runs (can be template) included

--- clfGraph/data/dl_script.py
+++ clfGraph/data/dl_script.py
@@ -5,63 +5,18 @@ Download your dataset in parts
 from dataclasses import dataclass
 import requests
 import os
+from src.custom_logging import configure_logging
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from typing import List, Optional, Union
+from typing import List, Optional, Union   
+from dotenv import load_dotenv
+load_dotenv() 
 
-@dataclass
-class DownloadSession:
-    project_name: str
-    file_list: Union[List[str], List[int]]
-    dataset_name: str
-    max_workers: int = 5
-    base_url: str
-    destination_folder: str
-    chunk_size: int = 3
-    max_retries: int = 3
-    train_test: Union["train", "test" | "all"]
+##### SET CONFIG #####
+LOG_LEVEL= os.environ("LOG_LEVEL", "debug")
+logger = configure_logging(LOG_LEVEL)
 
-def download_chunk_parallel(self, file_list):
-        for i, file_name in enumerate(self.file_list):
-            retries = 3
-            while retries < self.max_retries:
-                    try:
-                    
-                        with ThreadPoolExecutor(max_workers=5) as executor:
-                            futures = []
-                            for i in range(0, len(file_list) + 1, self.chunk_size):
-                                start_index = i
-                                end_index = min(i + self.chunk_size, len(file_list + 1))
-                                futures.append(executor.submit(self.download_chunk, start_index, end_index))
-                            for future in futures:
-                                future.result()  # Wait for all downloads to complete
-                                
-                                retries += 1
-                                print(f"Failed to download {file_name}. Retry {retries}/{self.max_retries}. Error: {e}")
-                    except requests.exceptions.RequestException as e:
-                        print(f"Failed to download {file_name}. Error: {e}")
-                    if retries == self.max_retries:
-                        print(f"Failed to download {file_name} after {self.max_retries} retries.")
-
-def download_all_parallel(self, file_list):
-    total_files = len(self.file_list)
-    def download_all_parallel(self):
-        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-            futures = {
-                executor.submit(self.download_file, file_name): file_name
-                for file_name in self.file_list
-            }
-            for future in as_completed(futures):
-                file_name = futures[future]
-                try:
-                    result = future.result()
-                    if result:
-                        print(f"Successfully downloaded {file_name} in parallel.")
-                    else:
-                        print(f"Failed to download {file_name} in parallel.")
-                except Exception as e:
-                    print(f"Exception occurred while downloading {file_name}: {e}")
-
-# Example usage:
+# List of Files to Download
+# Can call all or this list
 file_list = ["TCP_IP-DDoS-UDP5_train.pcap.csv",
     "TCP_IP-DDoS-UDP6_train.pcap.csv",
     "TCP_IP-DDoS-UDP7_train.pcap.csv",
@@ -84,12 +39,96 @@ file_list = ["TCP_IP-DDoS-UDP5_train.pcap.csv",
     "TCP_IP-DoS-UDP4_train.pcap.csv"
 ]
 
-DL_TYPE="train"
 
-if os.path.exists(f"../dataset/{dest_folder}/{session.train_test}"):
-    print(f"Folder {dest_folder}/{session.train_test} already exists")
-else:
-    os.makedirs(f"../dataset/{dest_folder}/{session.train_test}")
+'''
+Represents a download session for a specific project and dataset.
+
+Parameters:
+- project_name (str): The name of the project.
+- file_list (Union[List[str], List[int]): List of files to download.
+- dataset_name (str): The name of the dataset.
+- max_workers (int): Maximum number of workers for parallel downloading (default is 5).
+- base_url (str): The base URL for downloading the files.
+- destination_folder (str): The folder where the files will be saved.
+- chunk_size (int): Size of each download chunk (default is 3).
+- max_retries (int): Maximum number of retries in case of download failure (default is 3).
+- parallel (bool): Flag to enable parallel downloading (default is True).
+- train_test (Union[str, str, str]): Train and test dataset identifiers.
+'''
+@dataclass
+class DownloadSession:
+    project_name: str
+    file_list: Union[List[str], List[int]]
+    dataset_name: str
+    max_workers: int = 5
+    base_url: str
+    destination_folder: str
+    chunk_size: int = 3
+    max_retries: int = 3
+    parallel: bool = True
+    train_test: Union[str, str, str]
+
+'''
+Download files in chunks in parallel using ThreadPoolExecutor.
+
+Parameters:
+- file_list (List): List of files to download in chunks.
+
+Returns:
+- None
+'''
+def download_chunk_parallel(self, file_list):
+    for i, file_name in enumerate(self.file_list):
+        retries = 3
+        while retries < self.max_retries:
+            try:
+                with ThreadPoolExecutor(max_workers=5) as executor:
+                    futures = []
+                    for i in range(0, len(file_list) + 1, self.chunk_size):
+                        start_index = i
+                        end_index = min(i + self.chunk_size, len(file_list + 1))
+                        futures.append(executor.submit(self.download_chunk, start_index, end_index))
+                    for future in futures:
+                        future.result()  # Wait for all downloads to complete
+                        
+                        retries += 1
+                        print(f"Failed to download {file_name}. Retry {retries}/{self.max_retries}. Error: {e}")
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to download {file_name}. Error: {e}")
+            if retries == self.max_retries:
+                print(f"Failed to download {file_name} after {self.max_retries} retries.")
+
+'''
+Download all files at base_url in parallel using ThreadPoolExecutor.
+
+Parameters:
+- file_list (List): List of files to download in chunks.
+
+Returns:
+- None
+'''
+def download_all_parallel(self, file_list):
+    total_files = len(file_list)
+    def download_all_parallel(self):
+        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            futures = {
+                executor.submit(self.download_file, file_name): file_name
+                for file_name in self.file_list
+            }
+            for future in as_completed(futures):
+                file_name = futures[future]
+                try:
+                    result = future.result()
+                    if result:
+                        print(f"Successfully downloaded {file_name} in parallel.")
+                    else:
+                        print(f"Failed to download {file_name} in parallel.")
+                except Exception as e:
+                    print(f"Exception occurred while downloading {file_name}: {e}")
+
+
+
+DL_TYPE="train"
 
 download_session = DownloadSession(
     project_name="ClfGraph",
@@ -99,10 +138,17 @@ download_session = DownloadSession(
     base_url="http://205.174.165.80/IOTDataset/CICIoMT2024/Dataset/WiFI_and_MQTT/attacks/CSV/train",
     file_list=file_list,
     destination_folder=dest_folder,
-    chunk_size=3 
-    parallel=True
+    chunk_size=3, 
+    parallel=True,
     train_test="train"
 )
+
+if os.path.exists(f"../dataset/{project_name}/{download_session.train_test}"):
+    print(f"Folder {project_name}/{download_session.train_test} already exists")
+else:
+    os.makedirs(f"../dataset/{dest_folder}/{session.train_test}")
+
+
 
 if __name__ == "__main__":
     if DL_TYPE=="train" | DL_TYPE=="test":
--- clfGraph/src/custom_logging.py
+++ clfGraph/src/custom_logging.py
@@ -1,29 +1,35 @@
 from __future__ import annotations
 
 import logging
-
 from colorama import Fore, Style
+from datetime import date
+import os
 
 SIMPLE_LOG_FORMAT = "[%(asctime)s] %(levelname)s %(message)s"
 DEBUG_LOG_FORMAT = "[%(asctime)s] %(levelname)s %(filename)s:%(lineno)03d  %(message)s"
+LOG_LEVEL = logging.DEBUG
 
-
-def configure_logging(
-    level: int = logging.INFO,
-) -> None:
+def configure_logging(level: int = LOG_LEVEL) -> None:
     """Configure the native logging module."""
 
     # Auto-adjust default log format based on log level
     log_format = DEBUG_LOG_FORMAT if level == logging.DEBUG else SIMPLE_LOG_FORMAT
 
+    # Ensure log directory exists
+    log_dir = "./logs"
+    os.makedirs(log_dir, exist_ok=True)
+
     console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(f"{log_dir}/{__name__}_{date.today()}.log")
+    
     console_handler.setFormatter(FancyConsoleFormatter(log_format))
+    file_handler.setFormatter(logging.Formatter(log_format))
 
     # Configure the root logger
     logging.basicConfig(
         level=level,
         format=log_format,
-        handlers=[console_handler],
+        handlers=[console_handler, file_handler],
     )
 
 
@@ -38,7 +44,7 @@ class FancyConsoleFormatter(logging.Formatter):
     The color for each level is defined in the LEVEL_COLOR_MAP class attribute.
     """
 
-    # level -> (level & text color, title color)
+    # level -> (text color)
     LEVEL_COLOR_MAP = {
         logging.DEBUG: Fore.LIGHTBLACK_EX,
         logging.INFO: Fore.BLUE,
@@ -48,27 +54,33 @@ class FancyConsoleFormatter(logging.Formatter):
     }
 
     def format(self, record: logging.LogRecord) -> str:
-        # Make sure `msg` is a string
-        if not hasattr(record, "msg"):
-            record.msg = ""
-        elif not type(record.msg) is str:
+        # Ensure `msg` is a string
+        if not isinstance(record.msg, str):
             record.msg = str(record.msg)
 
         # Justify the level name to 5 characters minimum
         record.levelname = record.levelname.ljust(5)
 
         # Determine default color based on error level
-        level_color = ""
-        if record.levelno in self.LEVEL_COLOR_MAP:
-            level_color = self.LEVEL_COLOR_MAP[record.levelno]
-            record.levelname = f"{level_color}{record.levelname}{Style.RESET_ALL}"
+        level_color = self.LEVEL_COLOR_MAP.get(record.levelno, "")
+        record.levelname = f"{level_color}{record.levelname}{Style.RESET_ALL}"
 
         # Determine color for message
         color = getattr(record, "color", level_color)
         color_is_specified = hasattr(record, "color")
 
-        # Don't color INFO messages unless the color is explicitly specified.
+        # Don't color INFO messages unless the color is explicitly specified
         if color and (record.levelno != logging.INFO or color_is_specified):
             record.msg = f"{color}{record.msg}{Style.RESET_ALL}"
 
         return super().format(record)
+
+# Example usage:
+if __name__ == "__main__":
+    configure_logging()
+    logger = logging.getLogger(__name__)
+    logger.debug("This is a debug message.")
+    logger.info("This is an info message.")
+    logger.warning("This is a warning message.")
+    logger.error("This is an error message.")
+    logger.critical("This is a critical message.")
